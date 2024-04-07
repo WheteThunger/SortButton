6e254f79f7575d5cdb60566e935b287ab9bf2621
WhiteThunder <whetethunger@gmail.com>
2024-04-07 11:57:26 -0700

    Increment minor version

--- SortButton.cs
+++ SortButton.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Sort Button", "MON@H", "2.2.0")]
+    [Info("Sort Button", "MON@H", "2.3.0")]
     [Description("Adds a sort button to storage boxes, allowing you to sort items by name or category")]
     internal class SortButton : CovalencePlugin
     {
